4b27d12227724f711b3b58d318ea9fab66c40d70
t-l93 <153742131+t-l93@users.noreply.github.com>
2024-02-08 12:36:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,19 +4,18 @@ app = Flask(__name__) #creating flask app name
 
 @app.route('/')
 def home():
-    return render_template("index.html")
-
-@app.route('/resume_1')
-def resume_1():
-    return render_template("resume_1.html")
-
-@app.route('/resume_2')
-def resume_2():
     return render_template("resume_2.html")
 
-@app.route('/resume_template')
-def resume_template():
-    return render_template("resume_template.html")
+@app.route('/consultation/')
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
 
 if(__name__ == "__main__"):
     app.run()
